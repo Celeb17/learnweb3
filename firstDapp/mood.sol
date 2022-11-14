02a245ff019a61626480d2b002f925555917e7d4
wonk1994 <wonk1994@126.com>
2022-11-14 22:05:01 +0800

    提交first dapp

---
+++ firstDapp/mood.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: SimPL-2.0
+
+pragma solidity ^0.8.7;
+
+contract MoodDiary {
+    string mood;
+
+    function setMood(string memory _mood) public {
+        mood = _mood;
+    }
+
+    function getMood() public view returns (string memory) {
+        return mood;
+    }
+}
