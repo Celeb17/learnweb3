e5cdd66685937c30370758342c56a875cdd1da17
7782 <wangkai_7782@nesc.cn>
2022-11-16 20:57:13 +0800

    提交合约部署

--- NFT-Tutorial/hardhat.config.js
+++ NFT-Tutorial/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
 
 // Go to https://www.alchemyapi.io, sign up, create
 // a new App in its dashboard, and replace "KEY" with its key
@@ -8,11 +9,13 @@ const ALCHEMY_API_KEY = "Aes2ne9QdaaVd-NUEShOHsKwqnVNvo4a";
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Beware: NEVER put real Ether into testing accounts
-const GOERLI_PRIVATE_KEY = "";
+const GOERLI_PRIVATE_KEY = "f6014fef7b9c55058309faeb765c62be1a3e4b703e3b905494b217fda6b40eb5";
 
 module.exports = {
   solidity: "0.8.9",
+  defaultNetwork: "goerli",
   networks: {
+    hardhat: {},
     goerli: {
       url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
       accounts: [GOERLI_PRIVATE_KEY]
